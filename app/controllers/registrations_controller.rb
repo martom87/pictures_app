02cfd7ef5f91ced742033f015eca3432b1f855bc
martom87 <marcintomala87@gmail.com>
2019-01-27 22:46:24 +0100

    view for payment and stripe js for payment

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,7 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    build_resource(sign_up_params)
+  end
+
+end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,3 +7,10 @@ Rails.configuration.stripe = {
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+
+
+
+
+
+
